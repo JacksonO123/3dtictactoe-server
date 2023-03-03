3076143cec2707b0ff05b733abda6f111310d4cf
JacksonOtto <73965396+JacksonO123@users.noreply.github.com>
2023-03-02 18:00:15 -0800

    working connection, switch to typescript

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
-const express = require('express');
+import express from 'express';
 const port = process.env.PORT || 3000;
-const { Server } = require('ws');
-const { v4 } = require('uuid');
+import { Server, WebSocket } from 'ws';
+import { v4 as uuid } from 'uuid';
 
-const server = express().listen(port, () => console.log(`Listening on port ${port}`));
+const server = express().listen(port, () =>
+  console.log(`Listening on port ${port}`)
+);
 
 const wss = new Server({ server });
 
 let players = 0;
 
-let game = [];
+type charType = 'X' | 'O' | '';
+
+let game: charType[][][] = [];
 function resetGame() {
   game = [];
   for (let i = 0; i < 3; i++) {
@@ -24,55 +28,68 @@ function resetGame() {
 }
 resetGame();
 
-let sockets = {};
+type socketType = {
+  turn: boolean;
+  socket: WebSocket;
+  char: charType;
+  id: string;
+};
 
-function setTurn(key) {
-  let keys = Object.keys(sockets);
+const waiting = new Map<string, socketType>();
+const sockets = new Map<string, socketType>();
+
+function setTurn(key: string) {
+  const keys = [...sockets.keys()];
   let obj = {
-    req: 'turn_data',
+    req: 'turn-data',
     data: {
-      turn: false
-    }
-  }
+      turn: false,
+    },
+  };
   for (let i = 0; i < keys.length; i++) {
+    const player = sockets.get(key);
+    if (!player) continue;
     if (keys[i] === key) {
-      sockets[key].turn = true;
+      player.turn = true;
+      sockets.set(key, player);
       obj.data.turn = true;
-      sockets[key].socket.send(JSON.stringify(obj));
+      player.socket.send(JSON.stringify(obj));
     } else {
-      sockets[keys[i]].turn = false;
+      player.turn = false;
       obj.data.turn = false;
-      sockets[keys[i]].socket.send(JSON.stringify(obj));
+      player.socket.send(JSON.stringify(obj));
     }
   }
 }
 
 function startGame() {
-  const index = Math.floor(Math.random() * 2);
-  const keys = Object.keys(sockets);
-  setTurn(keys[index]);
+  const keys = [...sockets.keys()];
+  const player1 = sockets.get(keys[0]);
+  const player2 = sockets.get(keys[1]);
+  if (!player1 || !player2) return;
   let obj1 = {
     req: 'char',
     data: {
-      char: ''
-    }
-  }
+      char: '',
+    },
+  };
   obj1.data.char = 'X';
-  sockets[keys[0]].char = 'X';
-  sockets[keys[0]].socket.send(JSON.stringify(obj1));
+  player1.char = 'X';
+  player1.socket.send(JSON.stringify(obj1));
   obj1.data.char = 'O';
-  sockets[keys[1]].char = 'O';
-  sockets[keys[1]].socket.send(JSON.stringify(obj1));
+  player2.char = 'O';
+  player2.socket.send(JSON.stringify(obj1));
   const obj = {
-    req: 'game_data',
-    data: { game }
-  }
-  Object.values(sockets).forEach(socket => {
-    socket.socket.send(JSON.stringify(obj));
+    req: 'game-data',
+    data: { game },
+  };
+  [...sockets.keys()].forEach(key => {
+    sockets.get(key)?.socket.send(JSON.stringify(obj));
   });
+  setTurn(keys[Math.floor(Math.random() * 2)]);
 }
 
-function checkRow(row) {
+function checkRow(row: charType[]) {
   if (row[0] === row[1] && row[1] === row[2]) {
     if (row[0] !== '') {
       return row[0];
@@ -81,7 +98,7 @@ function checkRow(row) {
   return false;
 }
 
-function checkColumn(col, index) {
+function checkColumn(col: charType[][], index: number) {
   if (col[0][index] === col[1][index] && col[1][index] === col[2][index]) {
     if (col[0][index] !== '') {
       return col[0][index];
@@ -90,7 +107,7 @@ function checkColumn(col, index) {
   return false;
 }
 
-function checkRowZ(rowZ) {
+function checkRowZ(rowZ: charType[][][]) {
   for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
       if (rowZ[0][i][j] === rowZ[1][i][j] && rowZ[1][i][j] === rowZ[2][i][j]) {
@@ -103,13 +120,19 @@ function checkRowZ(rowZ) {
   return false;
 }
 
-function checkDiagonalZ(gameSlice) {
-  if (gameSlice[0][0] === gameSlice[1][1] && gameSlice[1][1] === gameSlice[2][2]) {
+function checkDiagonalZ(gameSlice: charType[][]) {
+  if (
+    gameSlice[0][0] === gameSlice[1][1] &&
+    gameSlice[1][1] === gameSlice[2][2]
+  ) {
     if (gameSlice[0][0] !== '') {
       return gameSlice[0][0];
     }
   }
-  if (gameSlice[0][2] === gameSlice[1][1] && gameSlice[1][1] === gameSlice[2][0]) {
+  if (
+    gameSlice[0][2] === gameSlice[1][1] &&
+    gameSlice[1][1] === gameSlice[2][0]
+  ) {
     if (gameSlice[0][2] !== '') {
       return gameSlice[0][2];
     }
@@ -117,14 +140,20 @@ function checkDiagonalZ(gameSlice) {
   return false;
 }
 
-function checkDiagonalX(gameClone) {
+function checkDiagonalX(gameClone: charType[][][]) {
   for (let i = 0; i < 3; i++) {
-    if (gameClone[2][0][i] == gameClone[1][1][i] && gameClone[1][1][i] == gameClone[0][2][i]) {
+    if (
+      gameClone[2][0][i] == gameClone[1][1][i] &&
+      gameClone[1][1][i] == gameClone[0][2][i]
+    ) {
       if (gameClone[2][0][i] !== '') {
         return gameClone[2][0][i];
       }
     }
-    if (gameClone[0][0][i] == gameClone[1][1][i] && gameClone[1][1][i] == gameClone[2][2][i]) {
+    if (
+      gameClone[0][0][i] == gameClone[1][1][i] &&
+      gameClone[1][1][i] == gameClone[2][2][i]
+    ) {
       if (gameClone[0][0][i] !== '') {
         return gameClone[0][0][i];
       }
@@ -133,14 +162,20 @@ function checkDiagonalX(gameClone) {
   return false;
 }
 
-function checkDiagonalY(gameClone) {
+function checkDiagonalY(gameClone: charType[][][]) {
   for (let i = 0; i < 3; i++) {
-    if (gameClone[0][i][0] == gameClone[1][i][1] && gameClone[1][i][1] == gameClone[2][i][2]) {
+    if (
+      gameClone[0][i][0] == gameClone[1][i][1] &&
+      gameClone[1][i][1] == gameClone[2][i][2]
+    ) {
       if (gameClone[0][i][0] !== '') {
         return gameClone[0][i][0];
       }
     }
-    if (gameClone[0][i][2] == gameClone[1][i][1] && gameClone[1][i][1] == gameClone[2][i][0]) {
+    if (
+      gameClone[0][i][2] == gameClone[1][i][1] &&
+      gameClone[1][i][1] == gameClone[2][i][0]
+    ) {
       if (gameClone[0][i][2] !== '') {
         return gameClone[0][i][2];
       }
@@ -182,80 +217,111 @@ function checkWinner() {
   let diagYWinner = checkDiagonalY(game);
   if (diagYWinner) return diagYWinner;
 
-  if (game[0][0][0] === game[1][1][1] && game[1][1][1] === game[2][2][2] && game[0][0][0] !== '') {
+  if (
+    game[0][0][0] === game[1][1][1] &&
+    game[1][1][1] === game[2][2][2] &&
+    game[0][0][0] !== ''
+  ) {
     return game[0][0][0];
   }
-  if (game[0][0][2] === game[1][1][1] && game[1][1][1] === game[2][2][0] && game[0][0][2] !== '') {
+  if (
+    game[0][0][2] === game[1][1][1] &&
+    game[1][1][1] === game[2][2][0] &&
+    game[0][0][2] !== ''
+  ) {
     return game[0][0][2];
   }
-  if (game[0][2][0] === game[1][1][1] && game[1][1][1] === game[2][0][2] && game[0][2][0] !== '') {
+  if (
+    game[0][2][0] === game[1][1][1] &&
+    game[1][1][1] === game[2][0][2] &&
+    game[0][2][0] !== ''
+  ) {
     return game[0][2][0];
   }
-  if (game[0][2][2] === game[1][1][1] && game[1][1][1] === game[2][0][0] && game[0][2][2] !== '') {
+  if (
+    game[0][2][2] === game[1][1][1] &&
+    game[1][1][1] === game[2][0][0] &&
+    game[0][2][2] !== ''
+  ) {
     return game[0][2][2];
   }
-  return false;
+  return null;
 }
 
-function getIdFromChar(char) {
-  let res;
-  Object.keys(sockets).forEach(key => {
-    if (sockets[key].char === char) {
-      res = key;
+function getIdFromChar(char: charType) {
+  const keys = [...sockets.keys()];
+  for (let i = 0; i < keys.length; i++) {
+    if (sockets.get(keys[i])?.char === char) {
+      return keys[i];
     }
-  });
-  return res;
+  }
+  return null;
 }
 
 function sendGameData() {
   const obj = {
-    req: 'game_data',
-    data: { game }
-  }
+    req: 'game-data',
+    data: { game },
+  };
   Object.values(sockets).forEach(socket => {
     socket.socket.send(JSON.stringify(obj));
   });
 }
 
-wss.on('connection', (ws) => {
+wss.on('connection', ws => {
   console.log('Client connected');
 
-  const socketId = v4();
+  const socketId = uuid();
 
-  const client = {
+  const client: socketType = {
     socket: ws,
     turn: false,
     char: '',
-    id: socketId
-  }
+    id: socketId,
+  };
 
   if (players < 2) {
-    sockets[socketId] = client;
+    sockets.set(socketId, client);
   } else {
     const obj = {
-      req: 'too_many_players',
+      req: 'too-many-players',
       data: {
         message: 'Too many players',
-      }
-    }
+      },
+    };
     ws.send(JSON.stringify(obj));
   }
   players++;
 
   if (players == 2) startGame();
 
-  ws.on('message', (msg) => {
-    msg = JSON.parse(msg);
+  type msgType = {
+    req: 'stay-alive' | 'play-data';
+    data: {
+      x: number;
+      y: number;
+      z: number;
+    };
+  };
+
+  ws.on('message', input => {
+    const msg = JSON.parse(input.toString()) as msgType;
     switch (msg.req) {
-      case 'stay-alive': break;
-      case 'play_data': {
-        if (sockets[socketId].turn) {
+      case 'stay-alive':
+        break;
+      case 'play-data': {
+        const player = sockets.get(socketId);
+        if (!player) break;
+        if (player.turn) {
           // update game object
           const { x, y, z } = msg.data;
           if (game[x][y][z] == '') {
             game[x][y][z] = client.char;
             // send game data to clients
-            const opponentKey = Object.keys(sockets).find(key => key !== socketId);
+            const opponentKey = [...sockets.keys()].find(
+              key => key !== socketId
+            );
+            if (!opponentKey) break;
             setTurn(opponentKey);
             sendGameData();
 
@@ -266,9 +332,9 @@ wss.on('connection', (ws) => {
                 const obj = {
                   req: 'winner',
                   data: {
-                    winner: winnerId == socket.id
-                  }
-                }
+                    winner: winnerId == socket.id,
+                  },
+                };
                 socket.socket.send(JSON.stringify(obj));
               });
             }
@@ -276,12 +342,13 @@ wss.on('connection', (ws) => {
         }
         break;
       }
-      default: break;
+      default:
+        break;
     }
   });
   ws.on('close', () => {
     console.log('Client disconnected');
-    delete sockets[socketId];
+    sockets.delete(socketId);
     players--;
     if (players < 2) {
       resetGame();
@@ -290,8 +357,8 @@ wss.on('connection', (ws) => {
   });
   function stayAliveLoop() {
     const obj = {
-      req: 'stay-alive'
-    }
+      req: 'stay-alive',
+    };
     ws.send(JSON.stringify(obj));
     setTimeout(() => {
       stayAliveLoop();
